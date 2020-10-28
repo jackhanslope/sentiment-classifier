67ef0ae7f09068dcbffea80eeebb96deb94a01c4
Jack Hanslope <code@jackhanslope.com>
2020-10-28 19:29:48 +0000

    Add todo functionality

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,8 +8,19 @@ class HelloWorld(Resource):
     def get(self):
         return {'ello': 'world'}
 
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
