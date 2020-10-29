4d434dedc91d3fe7a7c2b0f7b6931019c1083e72
Jack Hanslope <code@jackhanslope.com>
2020-10-29 17:55:47 +0000

    Update api file with model

--- api.py
+++ api.py
@@ -1,62 +1,33 @@
+from model import NLPModel
+
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-        'todo1': {'task': 'build an API'},
-        'todo2': {'task': 'be jack'},
-        'todo3': {'task': 'do some sailing'}
-        }
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="TODO {} doesn't exist".format(todo_id))
-
+model = NLPModel()
+model.load_vectorizer()
+model.load_clf()
 
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument('query')
 
-# Todo
-# shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-#TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+# PredictSentiment
+# Predicts the sentiment of an input review
+class PredictSentiment(Resource):
     def get(self):
-        return TODOS
-
-    def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        user_query = args['query']
 
+        prediction = model.predict([user_query])
+        text = 'Positive' if prediction == [1] else 'Negative'
 
-# setup API routing
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+        output = {'prediction': text}
 
+        return output
 
-
+api.add_resource(PredictSentiment, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
