142ec1cbd24afe3f1fa652850069ff847cf2d458
Jack Hanslope <code@jackhanslope.com>
2020-10-29 20:19:26 +0000

    Add basic public site

---
+++ public/js/sentiment.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('#senti-but').click(function () {
+    let str = 'query='.concat($('#review').val())
+
+    $.ajax({
+      url: 'http://127.0.0.1:5000/',
+      data: str,
+      type: 'GET',
+      success: function (response) {
+        if (response['prediction'] == 'Negative') {
+          $('body').css('background-color', '#ffa8a8')
+          $('textarea').css('background-color', '#ffa8a8')
+        } else if (response['prediction'] == 'Positive') {
+          $('body').css('background-color', '#b3ffa9')
+          $('textarea').css('background-color', '#b3ffa9')
+        }
+      }
+    });
+  });
+});
+
